3e4b7146f578167dabac54c563a9d6d2a52052f7
rela589n <zsidelnik@gmail.com>
2022-05-07 15:28:10 +0300

    create project

---
+++ src/Bundle/InjectBundle/Rela589nInjectBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rela589n\Bundle\InjectBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class Rela589nInjectBundle extends Bundle
+{
+}
